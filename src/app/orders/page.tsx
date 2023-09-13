145666421fa1494c098216b1f47536fa5c2ad689
SetroZ <yousfsbry66@gmail.com>
2023-09-13 13:08:04 +0800

    UI

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -1,9 +1,32 @@
 import React from 'react'
 
-const page = () => {
+export default function OrdersPage() {
   return (
-    <div>pages</div>
+    <div className='flex justify-center w-full items-center'>
+      <table className='w-full m-10 border-separate border-spacing-3 '>
+        <thead>
+          <tr className='text-left'>
+            <th>OrderID</th>
+            <th>Date</th>
+            <th>Price</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr className='text-sm md:text-base bg-gray-200 even:bg-red-400 '>
+            <td className='py-6 px-1  '>Test</td>
+            <td className='py-6 px-1'>Test</td>
+            <td className='py-6 px-1'>Test</td>
+            <td className='py-6 px-1'>Test</td>
+          </tr>
+          <tr className=' text-sm md:text-base bg-gray-200 even:bg-red-100 '>
+            <td className='py-6 px-1  '>Test</td>
+            <td className='py-6 px-1'>Test</td>
+            <td className='py-6 px-1'>Test</td>
+            <td className='py-6 px-1'>Test</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   )
 }
-
-export default page
--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import Counter from './Counter'
+import { singleProduct } from '@/data'
+import Image from 'next/image'
 
-const page = ({params}:{params:{id:string}}) => {
+const page = ({ params }: { params: { id: string } }) => {
   return (
-    <div>{params.id}</div>
+    <div className=' h-[calc(100vh-146px)] w-full px-2 gap-3 flex flex-col md:flex-row md:items-center'>
+      <div className='relative h-[60%] md:w-1/3'>
+        <Image
+          src={singleProduct.img as string}
+          fill
+          className='object-contain'
+          alt='product image'
+        />
+      </div>
+      <div className='  flex flex-col  text-red-500 gap-2 md:w-2/3'>
+        <h2 className=' uppercase font-bold text-2xl '>
+          {singleProduct.title}
+        </h2>
+        <p className='font-semibold'> {singleProduct.desc}</p>
+        <Counter {...singleProduct} />
+      </div>
+    </div>
   )
 }
 
