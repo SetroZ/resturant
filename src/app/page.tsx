adf65f55692d40365ff1886e3332f74099d357f0
SetroZ <yousfsbry66@gmail.com>
2023-09-15 13:38:16 +0800

    final

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Featured from '@/components/Featured'
 import Offer from '@/components/Offer'
 import Slider from '@/components/Slider'
@@ -9,20 +8,6 @@ export default function Home() {
       <Slider/>
       <Featured/>
       <Offer/>
-=======
-import Featured from '@/components/Main/Featured'
-import Offer from '@/components/Main/Offer'
-import Slider from '@/components/Main/Slider'
-import Image from 'next/image'
-
-export default function Home() {
-  return (
-    <main className="">
-      <Slider/>
-      <Featured/>
-      <Offer/>
-
->>>>>>> bafeda57f5e1fa8e7486b9ea5a01b80a8902d083
     </main>
   )
 }
