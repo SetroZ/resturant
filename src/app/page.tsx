050f5c9044d8261aa2a01ef43a6a2a9c882f2618
SetroZ <yousfsbry66@gmail.com>
2023-09-15 13:15:40 +0800

    merge conflicts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-import Featured from '@/components/Featured'
-import Offer from '@/components/Offer'
-import Slider from '@/components/Slider'
-
-export default function Home() {
-  return (
-    <main>
-      <Slider/>
-      <Featured/>
-      <Offer/>
-=======
 import Featured from '@/components/Main/Featured'
 import Offer from '@/components/Main/Offer'
 import Slider from '@/components/Main/Slider'
@@ -22,7 +10,6 @@ export default function Home() {
       <Featured/>
       <Offer/>
 
->>>>>>> bafeda57f5e1fa8e7486b9ea5a01b80a8902d083
     </main>
   )
 }
