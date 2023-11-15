9138ce8925a86c47ec554ee6556bb8774448b088
SetroZ <yousfsbry66@gmail.com>
2023-11-15 20:37:17 +0800

    removed for deployment

--- src/utils/getData.ts
+++ src/utils/getData.ts
@@ -1,13 +1,13 @@
 const getData = async <T>(url: string, options?: RequestInit) => {
-  // const res = await fetch(`${process.env.LOCATION}/api/${url}`, {
-  //   ...options,
-  //   //   next: { revalidate: 100 },
-  //   cache: 'no-cache',
-  // })
+  const res = await fetch(`${process.env.LOCATION}/api/${url}`, {
+    ...options,
+    //   next: { revalidate: 100 },
+    cache: 'no-cache',
+  })
 
-  // if (!res.ok) throw new Error('failed')
+  if (!res.ok) throw new Error('failed')
 
-  // return (await res.json()) as T
+  return (await res.json()) as T
 }
 
 export default getData
