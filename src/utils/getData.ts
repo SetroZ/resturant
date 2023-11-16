d88de5ba9ff1802ab9e96e943f61fbf944a33469
SetroZ <yousfsbry66@gmail.com>
2023-11-16 13:16:28 +0800

    test

--- src/utils/getData.ts
+++ src/utils/getData.ts
@@ -1,4 +1,5 @@
 const getData = async <T>(url: string, options?: RequestInit) => {
+  console.log(`${process.env.LOCATION}/api/${url}`)
   const res = await fetch(`${process.env.LOCATION}/api/${url}`, {
     ...options,
     //   next: { revalidate: 100 },
